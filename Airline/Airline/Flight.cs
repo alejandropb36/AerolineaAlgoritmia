f51f5d585265f1128850891d3d788d944aa0f407
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2017-08-20 14:37:00 -0500

    Creacion de proyecto, clases pasajero y vuelo

---
+++ Airline/Airline/Flight.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    class Flight
+    {
+        private char originCity;
+        private string route;
+        private int flightTime;
+        private int cost;
+        private char destinationCity;
+
+        public Flight()
+        {
+        }
+
+        public Flight(char originCity, string route, int flightTime, int cost, char destinationCity)
+        {
+            this.originCity = originCity;
+            this.route = route;
+            this.flightTime = flightTime;
+            this.cost = cost;
+            this.destinationCity = destinationCity;
+        }
+
+        public void setOriginCity(char originCity)
+        {
+            this.originCity = originCity;
+        }
+
+        public void setRoute(string route)
+        {
+            this.route = route;
+        }
+
+        public void setFlightTime(int flightTime)
+        {
+            this.flightTime = flightTime;
+        }
+
+        public void setCost(int cost)
+        {
+            this.cost = cost;
+        }
+
+        public void setDestinationCity(char destinationCity)
+        {
+            this.destinationCity = destinationCity;
+        }
+
+        public char getOriginCity()
+        {
+            return originCity;
+        }
+
+        public string getRoute()
+        {
+            return route;
+        }
+
+        public int getFlightTime()
+        {
+            return flightTime;
+        }
+
+        public int getCost()
+        {
+            return cost;
+        }
+
+        public char getDestinationCity()
+        {
+            return destinationCity;
+        }
+
+    }
+}
---
+++ Airline/Airline/Passenger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    class Passenger
+    {
+        private string name;
+        private string lastname;
+        private int age;
+        private int id;
+        // Vuelo
+        // Asiento
+
+        public Passenger()
+        {
+        }
+
+        public Passenger(string name, string lastname, int age, int id)
+        {
+            this.name = name;
+            this.lastname = lastname;
+            this.age = age;
+            this.id = id;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public void setLastname(string lastname)
+        {
+            this.lastname = lastname;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getLastname()
+        {
+            return lastname;
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+        public int getId()
+        {
+            return id;
+        }
+    }
+}
