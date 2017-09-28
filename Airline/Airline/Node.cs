e450b94cbf43671ddb707ff5268934c19b121828
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2017-09-27 20:40:56 -0500

    Creacion de clases para el grafo

---
+++ Airline/Airline/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airline
+{
+    public class Node
+    {
+        string city;
+        List<Adjacent> adjacencyList;
+    }
+}
