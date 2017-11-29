a11505ebfc31e6897c731b53f8f70d2855a7d8a7
Alejandro Ponce <alexponce36@hotmail.com>
2017-11-28 19:50:39 -0600

    Creacion del objeto dijkstra

--- Airline/Airline/DijkstraObject.cs
+++ Airline/Airline/DijkstraObject.cs
@@ -8,11 +8,55 @@ namespace Airline
 {
     public class DijkstraObject
     {
-        int costo;
-        int proveniente;
-        
+        int peso;
+        Node nodo;
+        bool definitivo;
+        DijkstraObject proveniente;
 
+        public DijkstraObject()
+        {
+            peso = 1000000000;
+            definitivo = false;
+        }
 
+        public void setPeso(int peso)
+        {
+            this.peso = peso;
+        }
 
+        public void setNodo(Node nodo)
+        {
+            this.nodo = nodo;
+        }
+
+        public void setDefinitivo(bool definitivo)
+        {
+            this.definitivo = definitivo;
+        }
+
+        public void setProveniente(DijkstraObject proveniente)
+        {
+            this.proveniente = proveniente;
+        }
+
+        public int getPeso()
+        {
+            return peso;
+        }
+
+        public Node getNodo()
+        {
+            return nodo;
+        }
+
+        public bool getDefinitivo()
+        {
+            return definitivo;
+        }
+
+        public DijkstraObject getProveniente()
+        {
+            return proveniente;
+        }
     }
 }
